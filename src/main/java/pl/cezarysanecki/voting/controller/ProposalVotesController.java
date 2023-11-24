2f4859ee23ebf87b2e326b1d21c0c467e1dafed8
Cezary Sanecki <saneckicezary@gmail.com>
2023-11-24 15:12:41 +0100

    Added controller

---
+++ src/main/java/pl/cezarysanecki/voting/controller/ProposalVotesController.java
@@ -0,0 +1,39 @@
+package pl.cezarysanecki.voting.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/voting/proposal")
+class ProposalVotesController {
+
+  @PostMapping
+  void createProposal() {
+
+  }
+
+  @PutMapping
+  void editProposal() {
+
+  }
+
+  @DeleteMapping
+  void deleteProposal() {
+
+  }
+
+  @GetMapping
+  void getProposal() {
+
+  }
+
+  @GetMapping
+  void getProposals() {
+
+  }
+
+}
