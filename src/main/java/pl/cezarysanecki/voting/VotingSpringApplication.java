2a729b18366c8ddae56af8afbdc7df416155acc0
Cezary Sanecki <saneckicezary@gmail.com>
2023-11-24 14:44:45 +0100

    Added spring

---
+++ src/main/java/pl/cezarysanecki/voting/VotingSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.cezarysanecki.voting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSpringApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VotingSpringApplication.class, args);
+  }
+
+}
---
+++ src/test/java/pl/cezarysanecki/voting/VotingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.cezarysanecki.voting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSpringApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
